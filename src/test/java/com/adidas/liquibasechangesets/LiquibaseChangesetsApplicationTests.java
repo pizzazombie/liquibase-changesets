1a08e0d3bc2bf2545cc05a3ffbb92e09457033e2
Stakheev, Lev [External] <Lev.Stakheev@externals.adidas.com>
2022-05-05 18:06:38 +0300

    add changesets

---
+++ src/test/java/com/adidas/liquibasechangesets/LiquibaseChangesetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidas.liquibasechangesets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseChangesetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
